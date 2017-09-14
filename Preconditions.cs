82ee889e03ed6e36f1d0cd5ffb0164a30e9235a8
Sirush <delaportemathieu@hotmail.com>
2017-09-14 22:44:02 +0200

    Fixed preconditions

--- Preconditions.cs
+++ Preconditions.cs
@@ -12,14 +12,13 @@ namespace DiscordBot
     {
         public override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IServiceProvider services)
         {
-            SocketGuild udh = await context.Client.GetGuildAsync(204951876960124928);
-            SocketGuildUser user = udh.GetUser(context.User.Id);
+            SocketGuildUser user = context.Message.Author as SocketGuildUser;
             
             if (user.Roles.Any(x => x.Id == 228015486120624130))
             {
-                return PreconditionResult.FromSuccess();
+                return Task.FromResult(PreconditionResult.FromSuccess());
             }
-            return PreconditionResult.FromError(user + " attempted to use admin only command!");
+            return Task.FromResult(PreconditionResult.FromError(user + " attempted to use admin only command!"));
         }
     }
 }
