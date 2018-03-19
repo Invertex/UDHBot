b1b09c42bc7d9de37ca5e92281da1881fbc2c1b5
Invertex <mitch.zais@gmail.com>
2018-03-19 05:54:53 -0700

    Minor naming fix in TaskExtension

--- Extensions/TaskExtensions.cs
+++ Extensions/TaskExtensions.cs
@@ -6,9 +6,9 @@ namespace DiscordBot.Extensions
 {
     public static class TaskExtensions
     {
-        public static Task DeleteAfterTime(this IDeletable message, int seconds = 0, int minutes = 0, int hours = 0, int days = 0) => message.DeleteAfterSeconds(new TimeSpan(seconds, minutes, hours, days));
-        public static Task DeleteAfterSeconds(this IDeletable message, double seconds) => message.DeleteAfterSeconds(TimeSpan.FromSeconds(seconds));
-        public static Task DeleteAfterSeconds(this IDeletable message, TimeSpan timeSpan)
+        public static Task DeleteAfterTime(this IDeletable message, int seconds = 0, int minutes = 0, int hours = 0, int days = 0) => message.DeleteAfterTimeSpan(new TimeSpan(seconds, minutes, hours, days));
+        public static Task DeleteAfterSeconds(this IDeletable message, double seconds) => message.DeleteAfterTimeSpan(TimeSpan.FromSeconds(seconds));
+        public static Task DeleteAfterTimeSpan(this IDeletable message, TimeSpan timeSpan)
         {
             return Task.Delay(timeSpan).ContinueWith(async _ => await message.DeleteAsync());
         }
@@ -17,7 +17,7 @@ namespace DiscordBot.Extensions
         public static Task DeleteAfterSeconds<T>(this Task<T> task, double seconds, bool awaitDeletion = false) where T : IDeletable => task.DeleteAfterTimeSpan(TimeSpan.FromSeconds(seconds), awaitDeletion);
         public static Task DeleteAfterTimeSpan<T>(this Task<T> task, TimeSpan timeSpan, bool awaitDeletion = false) where T : IDeletable
         {
-            var deletion = Task.Run(async () => await (await task).DeleteAfterSeconds(timeSpan));
+            var deletion = Task.Run(async () => await (await task).DeleteAfterTimeSpan(timeSpan));
             return awaitDeletion ? deletion : task;
         }
     }
