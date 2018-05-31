6e542cdfeb46369eb58dd477998ebef8de7f3511
Invertex <mitch.zais@gmail.com>
2018-05-31 08:07:42 -0700

    Attempt at fixing the issue of not unmuting if message is deleted before.

--- Extensions/TaskExtensions.cs
+++ Extensions/TaskExtensions.cs
@@ -7,18 +7,18 @@ namespace DiscordBot.Extensions
 {
     public static class TaskExtensions
     {
-        public static Task DeleteAfterTime(this IDeletable message, int seconds = 0, int minutes = 0, int hours = 0, int days = 0) => message.DeleteAfterTimeSpan(new TimeSpan(days, hours, minutes, seconds));
-        public static Task DeleteAfterSeconds(this IDeletable message, double seconds) => message.DeleteAfterTimeSpan(TimeSpan.FromSeconds(seconds));
+        public static Task DeleteAfterTime(this IDeletable message, int seconds = 0, int minutes = 0, int hours = 0, int days = 0) => message?.DeleteAfterTimeSpan(new TimeSpan(days, hours, minutes, seconds));
+        public static Task DeleteAfterSeconds(this IDeletable message, double seconds) => message?.DeleteAfterTimeSpan(TimeSpan.FromSeconds(seconds));
         public static Task DeleteAfterTimeSpan(this IDeletable message, TimeSpan timeSpan)
         {
-            return Task.Delay(timeSpan).ContinueWith(async _ => await message.DeleteAsync());
+            return Task.Delay(timeSpan).ContinueWith(async _ => await message?.DeleteAsync());
         }
 
-        public static Task DeleteAfterTime<T>(this Task<T> task, int seconds = 0, int minutes = 0, int hours = 0, int days = 0, bool awaitDeletion = false) where T : IDeletable => task.DeleteAfterTimeSpan(new TimeSpan(days, hours, minutes, seconds), awaitDeletion);
-        public static Task DeleteAfterSeconds<T>(this Task<T> task, double seconds, bool awaitDeletion = false) where T : IDeletable => task.DeleteAfterTimeSpan(TimeSpan.FromSeconds(seconds), awaitDeletion);
+        public static Task DeleteAfterTime<T>(this Task<T> task, int seconds = 0, int minutes = 0, int hours = 0, int days = 0, bool awaitDeletion = false) where T : IDeletable => task?.DeleteAfterTimeSpan(new TimeSpan(days, hours, minutes, seconds), awaitDeletion);
+        public static Task DeleteAfterSeconds<T>(this Task<T> task, double seconds, bool awaitDeletion = false) where T : IDeletable => task?.DeleteAfterTimeSpan(TimeSpan.FromSeconds(seconds), awaitDeletion);
         public static Task DeleteAfterTimeSpan<T>(this Task<T> task, TimeSpan timeSpan, bool awaitDeletion = false) where T : IDeletable
         {
-            var deletion = Task.Run(async () => await (await task).DeleteAfterTimeSpan(timeSpan));
+            var deletion = Task.Run(async () => await (await task)?.DeleteAfterTimeSpan(timeSpan));
             return awaitDeletion ? deletion : task;
         }
 
