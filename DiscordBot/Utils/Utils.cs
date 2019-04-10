90b4969dbd65c13382f9fe884318a35ea5893cfc
Invertex <mitch.zais@gmail.com>
2019-04-10 04:27:21 -0700

    Fix for birthday stuff by using Levenshtein difference algorithm

--- DiscordBot/Utils/Utils.cs
+++ DiscordBot/Utils/Utils.cs
@@ -96,4 +96,39 @@ public static class Utils
             (character >= 0x10000 && character <= 0x10FFFF)
         );
     }
+
+    /// <summary>
+    /// Levenshtein string distance algorithm.
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="t"></param>
+    /// <returns>Returns integer of how many edits required for the two strings to match.</returns>
+    public static int Difference(this string s, string t)
+    {
+        int n = s.Length;
+        int m = t.Length;
+
+        if (n == 0) { return m; }
+        if (m == 0) { return n; }
+
+        int[,] d = new int[n + 1, m + 1];
+
+        //Initialize arrays with values
+        for (int i = 0; i <= n; d[i, 0] = i++){}
+        for (int j = 0; j <= m; d[0, j] = j++){}
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+                d[i, j] = Math.Min(
+                    Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                    d[i - 1, j - 1] + cost);
+            }
+        }
+
+        return d[n, m];
+    }
 }
