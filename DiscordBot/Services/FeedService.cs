b74d99c5e26b14b414ff0ecfe68f3383e1eb80a4
Pierre Demessence <pierre.demessence@gmail.com>
2020-10-22 17:21:33 +0200

    Removed code duplication

--- DiscordBot/Services/FeedService.cs
+++ DiscordBot/Services/FeedService.cs
@@ -28,11 +28,11 @@ namespace DiscordBot.Services
             _client = client;
         }
 
-        public async void CheckUnityBetas(FeedData feedData)
+        public async void HandleFeed(FeedData feedData, string url, string channelID, string roleID, string message)
         {
             try
             {
-                HttpWebRequest webRequest = (HttpWebRequest) WebRequest.Create(BETA_URL);
+                HttpWebRequest webRequest = (HttpWebRequest) WebRequest.Create(url);
                 HttpWebResponse webResponse = (HttpWebResponse) webRequest.GetResponse();
                 Stream dataStream = webResponse.GetResponseStream();
                 StreamReader streamReader = new StreamReader(dataStream, Encoding.UTF8);
@@ -42,8 +42,8 @@ namespace DiscordBot.Services
                 XmlReader reader = new XmlTextReader(new StringReader(response));
 
                 SyndicationFeed feed = SyndicationFeed.Load(reader);
-                var channel = _client.GetChannel(_settings.UnityNewsChannel.Id) as ISocketMessageChannel;
-                var role = _client.GetGuild(_settings.guildId).GetRole(_settings.SubsReleasesRoleId);
+                var channel = _client.GetChannel(channelID) as ISocketMessageChannel;
+                var role = _client.GetGuild(_settings.guildId).GetRole(roleID);
                 foreach (var item in feed.Items.Take(MAXIMUM_CHECK))
                 {
                     if (!feedData.PostedIds.Contains(item.Id))
@@ -51,8 +51,7 @@ namespace DiscordBot.Services
                         feedData.PostedIds.Add(item.Id);
 
                         await role.ModifyAsync(properties => { properties.Mentionable = true; });
-                        string message =
-                            $"{role.Mention} New unity **beta **release !** {item.Title.Text}** \n <{item.Links[0].Uri.ToString().Replace("/fr/", "/")}>";
+                        string messageToSend = string.Format(message, role.Mention, item.Title.Text, item.Links[0].Uri.ToString().Replace("/fr/", "/"));
 
                         await channel.SendMessageAsync(message);
                         await role.ModifyAsync(properties => { properties.Mentionable = false; });
@@ -63,81 +62,21 @@ namespace DiscordBot.Services
             {
                 Console.WriteLine(e);
             }
+        }
+
+        public async void CheckUnityBetas(FeedData feedData)
+        {
+            await this.HandleFeed(feedData, BETA_URL, _settings.UnityNewsChannel.Id, _settings.SubsReleasesRoleId, $"{0} New unity **beta **release !** {1}** \n <{2}>");
         }
 
         public async void CheckUnityReleases(FeedData feedData)
         {
-            try
-            {
-                HttpWebRequest webRequest = (HttpWebRequest) WebRequest.Create(RELEASE_URL);
-                HttpWebResponse webResponse = (HttpWebResponse) webRequest.GetResponse();
-                Stream dataStream = webResponse.GetResponseStream();
-                StreamReader streamReader = new StreamReader(dataStream, Encoding.UTF8);
-                string response = streamReader.ReadToEnd();
-                streamReader.Close();
-                response = Utils.SanitizeXml(response);
-                XmlReader reader = new XmlTextReader(new StringReader(response));
-
-                SyndicationFeed feed = SyndicationFeed.Load(reader);
-                var channel = _client.GetChannel(_settings.UnityNewsChannel.Id) as ISocketMessageChannel;
-                var role = _client.GetGuild(_settings.guildId).GetRole(_settings.SubsReleasesRoleId);
-
-                foreach (var item in feed.Items.Take(MAXIMUM_CHECK))
-                {
-                    if (!feedData.PostedIds.Contains(item.Id))
-                    {
-                        feedData.PostedIds.Add(item.Id);
-
-                        await role.ModifyAsync(properties => { properties.Mentionable = true; });
-                        string message =
-                            $"{role.Mention} New unity release ! **{item.Title.Text}** \n <{item.Links[0].Uri.ToString().Replace("/fr/", "/")}>";
-
-                        await channel.SendMessageAsync(message);
-                        await role.ModifyAsync(properties => { properties.Mentionable = false; });
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            await this.HandleFeed(feedData, RELEASE_URL, _settings.UnityNewsChannel.Id, _settings.SubsReleasesRoleId, $"{0} New unity release ! **{1}** \n <{2}>");
         }
 
         public async void CheckUnityBlog(FeedData feedData)
         {
-            try
-            {
-                HttpWebRequest webRequest = (HttpWebRequest) WebRequest.Create(BLOG_URL);
-                HttpWebResponse webResponse = (HttpWebResponse) webRequest.GetResponse();
-                Stream dataStream = webResponse.GetResponseStream();
-                StreamReader streamReader = new StreamReader(dataStream, Encoding.UTF8);
-                string response = streamReader.ReadToEnd();
-                streamReader.Close();
-                response = Utils.SanitizeXml(response);
-                XmlReader reader = new XmlTextReader(new StringReader(response));
-
-                SyndicationFeed feed = SyndicationFeed.Load(reader);
-                var channel = _client.GetChannel(_settings.UnityNewsChannel.Id) as ISocketMessageChannel;
-                var role = _client.GetGuild(_settings.guildId).GetRole(_settings.SubsNewsRoleId);
-                foreach (var item in feed.Items.Take(MAXIMUM_CHECK))
-                {
-                    if (!feedData.PostedIds.Contains(item.Id))
-                    {
-                        feedData.PostedIds.Add(item.Id);
-
-                        await role.ModifyAsync(properties => { properties.Mentionable = true; });
-                        string message =
-                            $"{role.Mention} New unity blog post ! **{item.Title.Text}**\n{item.Links[0].Uri.ToString().Replace("/fr/", "/")}";
-
-                        await channel.SendMessageAsync(message);
-                        await role.ModifyAsync(properties => { properties.Mentionable = false; });
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            await this.HandleFeed(feedData, BLOG_URL, _settings.UnityNewsChannel.Id, _settings.SubsNewsRoleId, $"{0} New unity blog post ! **{1}**\n{2}");
         }
     }
 }
