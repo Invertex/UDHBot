e807e5355dbd315e9e0d0b702d462f5129ba0f48
Pierre Demessence <pierre.demessence@gmail.com>
2020-10-25 02:44:10 +0100

    Removed commented out code

--- DiscordBot/Preconditions.cs
+++ DiscordBot/Preconditions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,17 +10,6 @@ namespace DiscordBot
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
     public class RequireAdminAttribute : PreconditionAttribute
     {
-       /* public override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IServiceProvider services)
-        {
-                    SocketGuildUser user = context.Message.Author as SocketGuildUser;
-            
-            if (user.Roles.Any(x => x.Id == 228015486120624130))
-            {
-                return Task.FromResult(PreconditionResult.FromSuccess());
-            }
-            return Task.FromResult(PreconditionResult.FromError(user + " attempted to use admin only command!"));
-        }*/
-
         public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
         {
             SocketGuildUser user = context.Message.Author as SocketGuildUser;
@@ -36,17 +25,6 @@ namespace DiscordBot
         [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
     public class RequireStaffAttribute : PreconditionAttribute
     {
-       /* public override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IServiceProvider services)
-        {
-                    SocketGuildUser user = context.Message.Author as SocketGuildUser;
-            
-            if (user.Roles.Any(x => x.Id == 228015486120624130))
-            {
-                return Task.FromResult(PreconditionResult.FromSuccess());
-            }
-            return Task.FromResult(PreconditionResult.FromError(user + " attempted to use admin only command!"));
-        }*/
-
         public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
         {
             SocketGuildUser user = context.Message.Author as SocketGuildUser;
