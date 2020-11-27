727421c53c8cad40327a27ca583e1e8a6c7043ab
Paul Capra <c.paulica@gmail.com>
2020-11-27 17:15:17 +0200

    Moved events to the appropriate services
    Created ModerationService for deleted messages logging
    
    Moved user related events to UserService

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -20,9 +20,6 @@ namespace DiscordBot
 
         private CommandService _commandService;
         private IServiceProvider _services;
-        private ILoggingService _loggingService;
-        private DatabaseService _databaseService;
-        private UserService _userService;
         private CommandHandlingService _commandHandlingService;
 
         private static PayWork _payWork;
@@ -48,10 +45,8 @@ namespace DiscordBot
             });
 
             _services = ConfigureServices();
-            _loggingService = _services.GetRequiredService<ILoggingService>();
-            _databaseService = _services.GetRequiredService<DatabaseService>();
-            _userService = _services.GetRequiredService<UserService>();
             _commandHandlingService = _services.GetRequiredService<CommandHandlingService>();
+            _services.GetRequiredService<ModerationService>();
             
             await _commandHandlingService.Initialize();
 
@@ -59,11 +54,6 @@ namespace DiscordBot
 
             await _client.LoginAsync(TokenType.Bot, _settings.Token);
             await _client.StartAsync();
-
-            _client.MessageDeleted += MessageDeleted;
-            _client.UserJoined += UserJoined;
-            _client.GuildMemberUpdated += UserUpdated;
-            _client.UserLeft += UserLeft;
 
             _client.Ready += () =>
             {
@@ -78,22 +68,23 @@ namespace DiscordBot
         private IServiceProvider ConfigureServices()
         {
             return new ServiceCollection()
-                .AddSingleton(_settings)
-                .AddSingleton(_rules)
+                .AddSingleton(_settings)//
+                .AddSingleton(_rules)//
                 .AddSingleton(_payWork)
                 .AddSingleton(_userSettings)
-                .AddSingleton(_client)
-                .AddSingleton(_commandService)
-                .AddSingleton<CommandHandlingService>()
-                .AddSingleton<ILoggingService, LoggingService>()
-                .AddSingleton<DatabaseService>()
-                .AddSingleton<UserService>()
-                .AddSingleton<PublisherService>()
+                .AddSingleton(_client)//
+                .AddSingleton(_commandService)//
+                .AddSingleton<CommandHandlingService>()//
+                .AddSingleton<ILoggingService, LoggingService>()//
+                .AddSingleton<DatabaseService>()//
+                .AddSingleton<UserService>()//
+                .AddSingleton<ModerationService>()//
+                .AddSingleton<PublisherService>()//
                 .AddSingleton<FeedService>()
-                .AddSingleton<UpdateService>()
+                .AddSingleton<UpdateService>()//
                 .AddSingleton<AudioService>()
-                .AddSingleton<AnimeService>()
-                .AddSingleton<CurrencyService>()
+                .AddSingleton<AnimeService>()//
+                .AddSingleton<CurrencyService>()//
                 .BuildServiceProvider();
         }
 
@@ -122,107 +113,6 @@ namespace DiscordBot
             Console.ForegroundColor = cc;
             return Task.CompletedTask;
         }
-
-        private async Task MessageDeleted(Cacheable<IMessage, ulong> message, ISocketMessageChannel channel)
-        {
-            if (message.Value.Author.IsBot || channel.Id == _settings.BotAnnouncementChannel.Id)
-                return;
-
-            var content = message.Value.Content;
-            if (content.Length > 800)
-                content = content.Substring(0, 800);
-
-            EmbedBuilder builder = new EmbedBuilder()
-                .WithColor(new Color(200, 128, 128))
-                .WithTimestamp(message.Value.Timestamp)
-                .WithFooter(footer =>
-                {
-                    footer
-                        .WithText($"In channel {message.Value.Channel.Name}");
-                })
-                .WithAuthor(author =>
-                {
-                    author
-                        .WithName($"{message.Value.Author.Username}");
-                })
-                .AddField("Deleted message", content);
-            Embed embed = builder.Build();
-
-            await _loggingService.LogAction(
-                $"User {message.Value.Author.Username}#{message.Value.Author.DiscriminatorValue} has " +
-                $"deleted the message\n{content}\n from channel #{channel.Name}", true, false);
-            await _loggingService.LogAction(" ", false, true, embed);
-        }
-
-        private async Task UserJoined(SocketGuildUser user)
-        {
-            ulong general = _settings.GeneralChannel.Id;
-            var socketTextChannel = _client.GetChannel(general) as SocketTextChannel;
-
-            _databaseService.AddNewUser(user);
-
-            //Check for existing mute
-            if (_userService._mutedUsers.HasUser(user.Id))
-            {
-                await user.AddRoleAsync(socketTextChannel?.Guild.GetRole(_settings.MutedRoleId));
-                await _loggingService.LogAction(
-                    $"Currently muted user rejoined - {user.Mention} - `{user.Username}#{user.DiscriminatorValue}` - ID : `{user.Id}`");
-                await socketTextChannel.SendMessageAsync(
-                    $"{user.Mention} tried to rejoin the server to avoid their mute. Mute time increased by 72 hours.");
-                _userService._mutedUsers.AddCooldown(user.Id, hours: 72);
-                return;
-            }
-
-
-            await _loggingService.LogAction(
-                $"User Joined - {user.Mention} - `{user.Username}#{user.DiscriminatorValue}` - ID : `{user.Id}`");
-
-            Embed em = _userService.WelcomeMessage(user.GetAvatarUrl(), user.Username, user.DiscriminatorValue);
-
-            if (socketTextChannel != null)
-            {
-                await socketTextChannel.SendMessageAsync(string.Empty, false, em);
-            }
-
-            string globalRules = _rules.Channel.First(x => x.Id == 0).Content;
-            IDMChannel dm = await user.GetOrCreateDMChannelAsync();
-            await dm.SendMessageAsync(
-                "Hello and welcome to Unity Developer Community !\nHope you enjoy your stay.\nHere are some rules to respect to keep the community friendly, please read them carefully.\n" +
-                "Please also read the additional informations in the **#welcome** channel." +
-                "You can get all the available commands on the server by typing !help in the **#bot-commands** channel.");
-            await dm.SendMessageAsync(globalRules);
-
-            //TODO: add users when bot was offline
-        }
-
-        private async Task UserUpdated(SocketGuildUser oldUser, SocketGuildUser user)
-        {
-            if (oldUser.Nickname != user.Nickname)
-            {
-                await _loggingService.LogAction(
-                    $"User {oldUser.Nickname ?? oldUser.Username}#{oldUser.DiscriminatorValue} changed his " +
-                    $"username to {user.Nickname ?? user.Username}#{user.DiscriminatorValue}");
-                _databaseService.UpdateUserName(user.Id, user.Nickname);
-            }
-
-            if (oldUser.AvatarId != user.AvatarId)
-            {
-                var avatar = user.GetAvatarUrl();
-                _databaseService.UpdateUserAvatar(user.Id, avatar);
-            }
-        }
-
-        private async Task UserLeft(SocketGuildUser user)
-        {
-            DateTime joinDate;
-            DateTime.TryParse(_databaseService.GetUserJoinDate(user.Id), out joinDate);
-            TimeSpan timeStayed = DateTime.Now - joinDate;
-            await _loggingService.LogAction(
-                $"User Left - After {(timeStayed.Days > 1 ? Math.Floor((double) timeStayed.Days).ToString() + " days" : " ")}" +
-                $" {Math.Floor((double) timeStayed.Hours).ToString()} hours {user.Mention} - `{user.Username}#{user.DiscriminatorValue}` - ID : `{user.Id}`");
-            _databaseService.DeleteUser(user.Id);
-        }
-        
 
         private static void DeserializeSettings()
         {
