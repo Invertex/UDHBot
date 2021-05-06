6acb121c5b29ed410c05d72c4a65cf4bccbefe55
James Kellie <Jpk_GameID@hotmail.com>
2021-05-06 15:06:05 +1000

    Update ReactionRoleModule.cs
    - Improve summary for commands
    - Make methods public for module discovery

--- DiscordBot/Modules/ReactionRoleModule.cs
+++ DiscordBot/Modules/ReactionRoleModule.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using DiscordBot.Extensions;
 using DiscordBot.Services;
 using DiscordBot.Settings.Deserialized;
 
@@ -33,15 +34,15 @@ namespace DiscordBot.Modules
         [RequireUserPermission(GuildPermission.Administrator)]
         [Summary("Duration between bulk role changes in ms. Default: 5000")]
         [Command("Delay"), Priority(99)]
-        async Task SetReactRoleDelay(uint delay)
+        public async Task SetReactRoleDelay(uint delay)
         {
             if (_reactRoleService.SetReactRoleDelay(delay))
                 await Context.Message.AddReactionAsync(_thumbUpEmote);
         }
         [RequireUserPermission(GuildPermission.Administrator)]
-        [Summary("Log all updates, can get spammy. Default: false")]
+        [Summary("Log all updates, gets spammy. Default: false")]
         [Command("Log"), Priority(98)]
-        async Task SetReactLogState(bool state)
+        public async Task SetReactLogState(bool state)
         {
             if (_reactRoleService.SetReactLogState(state))
                 await Context.Message.AddReactionAsync(_thumbUpEmote);
@@ -50,9 +51,9 @@ namespace DiscordBot.Modules
 
         #region MessageMaking
         [RequireUserPermission(GuildPermission.Administrator)]
-        [Summary("Begins new reaction message setup.")]
+        [Summary("Begins message setup.")]
         [Command("NewMessage"), Priority(0)]
-        async Task NewMessageSetup(IMessageChannel channel, ulong messageId, string description = "")
+        public async Task NewMessageSetup(IMessageChannel channel, ulong messageId, string description = "")
         {
             if (_reactRoleService.IsPreparingMessage == true)
             {
@@ -87,7 +88,7 @@ namespace DiscordBot.Modules
         [RequireUserPermission(GuildPermission.Administrator)]
         [Summary("Allows making changes to previously saved messages.")]
         [Command("EditMessage"), Priority(1)]
-        async Task EditMessageSetup(ulong messageId, string description = "")
+        public async Task EditMessageSetup(ulong messageId, string description = "")
         {
             if (_reactRoleService.IsPreparingMessage == true)
             {
@@ -112,7 +113,7 @@ namespace DiscordBot.Modules
         [RequireUserPermission(GuildPermission.Administrator)]
         [Summary("Adds/Removes Roles/Emotes to the prepared message. Removes role if emoteId is 0")]
         [Command("Emote"), Priority(2)]
-        async Task AddEmoteRoles(IRole role, ulong emoteId = 0, string name = "")
+        public async Task AddEmoteRoles(IRole role, string emoteString = "", string name = "")
         {
             if (!_reactRoleService.IsPreparingMessage)
             {
@@ -154,7 +155,7 @@ namespace DiscordBot.Modules
         [RequireUserPermission(GuildPermission.Administrator)]
         [Summary("Preview the message being prepared.")]
         [Command("Preview"), Priority(3)]
-        async Task PreviewNewReactionMessage()
+        public async Task PreviewNewReactionMessage()
         {
             if (!_reactRoleService.IsPreparingMessage)
             {
@@ -174,7 +175,7 @@ namespace DiscordBot.Modules
         [RequireUserPermission(GuildPermission.Administrator)]
         [Summary("Cancel the current reaction roles.")]
         [Command("Cancel"), Priority(4)]
-        async Task CancelNewReactionMessage()
+        public async Task CancelNewReactionMessage()
         {
             if (!_reactRoleService.IsPreparingMessage)
             {
@@ -189,14 +190,14 @@ namespace DiscordBot.Modules
         [RequireUserPermission(GuildPermission.Administrator)]
         [Summary("Saves the message being prepared.")]
         [Command("Save"), Priority(10)]
-        async Task SaveNewReactionMessage()
+        public async Task SaveNewReactionMessage()
         {
             if (!_reactRoleService.IsPreparingMessage)
             {
                 await ReplyAsync("No message is being prepared, use NewMessage first!");
                 return;
             }
-            await ReplyAsync("Saving Values, use ***!reactrole restart*** to enable the changes.");
+            await ReplyAsync("Saving Values, use ***reactrole restart*** to enable the changes.");
             _reactRoleService.StoreNewMessage();
         }
         #endregion
@@ -206,7 +207,7 @@ namespace DiscordBot.Modules
         [RequireUserPermission(GuildPermission.Administrator)]
         [Summary("Delete a stored ReactionRole Configuration.")]
         [Command("DeleteConfig"), Priority(11)]
-        async Task DeleteReactionRoleConfig(uint messageId)
+        public async Task DeleteReactionRoleConfig(uint messageId)
         {
             var foundMessage = _reactRoleService.ReactSettings.UserReactRoleList.Find(reactMessage => reactMessage.MessageId == messageId);
             if (foundMessage == null)
@@ -222,7 +223,7 @@ namespace DiscordBot.Modules
         [RequireUserPermission(GuildPermission.Administrator)]
         [Summary("Lists out all current reaction messages.")]
         [Command("List"), Priority(80)]
-        async Task ListReactionRoleConfig()
+        public async Task ListReactionRoleConfig()
         {
             var messageList = _reactRoleService.ReactSettings.UserReactRoleList;
             if (messageList.Count == 0)
@@ -250,7 +251,7 @@ namespace DiscordBot.Modules
         [RequireUserPermission(GuildPermission.Administrator)]
         [Summary("Restarts the ReactRoleService.")]
         [Command("Restart"), Priority(90)]
-        async Task ReactRestartService()
+        public async Task ReactRestartService()
         {
             await ReplyAsync("Reaction role service is restarting.");
             var results = await _reactRoleService.Restart();
@@ -265,11 +266,12 @@ namespace DiscordBot.Modules
         [RequireUserPermission(GuildPermission.Administrator)]
         [Summary("Does what you see now.")]
         [Command("Help"), Priority(100)]
-        async Task ReactionRoleHelp()
+        public async Task ReactionRoleHelp()
         {
             await ReplyAsync(_commandList);
         }
 
+        /// <summary> Generates a command list that attempts to give the user valuable information, also provides argument information. </summary>
         public static void GenerateCommandList(CommandService commandService)
         {
             StringBuilder commandList = new StringBuilder();
