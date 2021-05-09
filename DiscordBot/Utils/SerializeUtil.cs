2ab27c346b7bf70019a77394251bbc8d7a38d1ba
James Kellie <Jpk_GameID@hotmail.com>
2021-05-10 02:07:03 +1000

    Logger and Serializer
    Simple log and serializer to simplify saving/loading data and log messages for debugging. The serializer should help clean up a lot of repeated code aswell.

---
+++ DiscordBot/Utils/SerializeUtil.cs
@@ -0,0 +1,43 @@
+using System.IO;
+using Newtonsoft.Json;
+//TODO async?
+public class SerializeUtil
+{
+    public static T DeserializeFile<T>(string path, bool newFileIfNotExists = true) where T : new()
+    {
+        T deserializedItem;
+        // Check if file exists,
+        if (!File.Exists(path))
+        {
+            if (newFileIfNotExists)
+            {
+                ConsoleLogger.Log(
+                    $@"Deserialized File at '{path}' does not exist, attempting to generate new file.",
+                    Severity.Warning);
+                deserializedItem = new T();
+                File.WriteAllText(path, JsonConvert.SerializeObject(deserializedItem));
+            }
+            else
+            {
+                ConsoleLogger.Log($@"Deserialized File at '{path}' does not exist.", Severity.Error);
+            }
+        }
+
+        using var file = File.OpenText(path);
+        return JsonConvert.DeserializeObject<T>(file.ReadToEnd());
+    }
+
+    /// <summary> Tests objectToSerialize to confirm not null before saving it to path. </summary>
+    public static bool SerializeFile<T>(string path, T objectToSerialize)
+    {
+        if (objectToSerialize == null)
+        {
+            ConsoleLogger.Log($"Object `{path}` passed into SerializeFile is null, ignoring save request.",
+                Severity.Warning);
+            return false;
+        }
+
+        File.WriteAllText(path, JsonConvert.SerializeObject(objectToSerialize));
+        return true;
+    }
+}
