7aee2bf5b0c8396790db2c1fb474d8e4bbd5b5d1
James Kellie <Jpk_GameID@hotmail.com>
2021-06-02 13:05:28 +1000

    Remove now unnecessary names for old database

--- DiscordBot/Extensions/UserDBRepository.cs
+++ DiscordBot/Extensions/UserDBRepository.cs
@@ -16,17 +16,11 @@ namespace DiscordBot.Extensions
         public string UserID { get; set; }
         public string Avatar { get; set; }
         public string AvatarUrl { get; set; }
-        // bot | muted | mutedTime
         public DateTime JoinDate { get; set; }
         public int Karma { get; set; }
         public int KarmaGiven { get; set; }
-        // profanity | karmaLimit | active | lastSeen | lastMessage
-        // status | roles | packagesAvailable | packagesLeft
-        // keycode
         public long Exp { get; set; }
         public int Level { get; set; }
-        // public int Rank { get; set; } // Given scale of server, this could probably be removed and done via query.
-        // game | live | liveMessage | udc
     }
     
     public interface IServerUserRepo
