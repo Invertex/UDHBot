59e29d35557d9abafa68c3bbec0354d43fc10d04
James Kellie <Jpk_GameID@hotmail.com>
2021-09-17 22:12:35 +1000

    Create MathUtility.cs
    New Math Utility that'll hopefully have a collection of common or potentially reusable math.

---
+++ DiscordBot/Utils/MathUtility.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace DiscordBot.Utils
+{
+    public static class MathUtility
+    {
+        public static float CelsiusToFahrenheit(float value)
+        {
+            return (float) Math.Round(value * 1.8f + 32, 2);
+        }
+
+        public static float FahrenheitToCelsius(float value)
+        {
+            return (float) Math.Round((value - 32) * 0.555555f, 2);
+        }
+    }
+}
