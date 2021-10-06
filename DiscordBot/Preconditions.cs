6a734c387a1801cb1c2811345c4f4a1e85b6df8b
Pierre Demessence <pierre.demessence@gmail.com>
2021-10-06 04:26:02 +0200

    Split the Precondition for threads into multiple smaller reusable ones

--- DiscordBot/Preconditions.cs
+++ DiscordBot/Preconditions.cs
@@ -53,28 +53,89 @@ namespace DiscordBot
     }
 
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class RequireAutoThreadAuthorAttribute : PreconditionAttribute
+    public class RequireThreadAttribute : PreconditionAttribute
+    {
+        protected SocketThreadChannel _currentThread;
+
+        public override async Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            this._currentThread = context.Message.Channel as SocketThreadChannel;
+            if (this._currentThread != null) return await Task.FromResult(PreconditionResult.FromSuccess());
+
+            Task task = context.Message.DeleteAfterSeconds(seconds: 10);
+            return await Task.FromResult(PreconditionResult.FromError("This command can only be used in a thread."));
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class RequireAutoThreadAttribute : RequireThreadAttribute
+    {
+        protected Settings.Deserialized.AutoThreadChannel _autoThreadChannel;
+
+        public override async Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            var res = await base.CheckPermissionsAsync(context, command, services);
+            if (!res.IsSuccess) return res;
+
+            var settings = services.GetRequiredService<Settings.Deserialized.Settings>();
+            this._autoThreadChannel = settings.AutoThreadChannels.Find(x => this._currentThread.ParentChannel.Id == x.Id);
+            if (this._autoThreadChannel != null) return await Task.FromResult(PreconditionResult.FromSuccess());
+
+            Task task = context.Message.DeleteAfterSeconds(seconds: 10);
+            return await Task.FromResult(PreconditionResult.FromError("This command can only be used in a thread created automatically."));
+
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class RequireArchivableAutoThreadAttribute : RequireAutoThreadAttribute
     {
         public override async Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
         {
-            var currentThread = context.Message.Channel as SocketThreadChannel;
-            if (currentThread == null) return await Task.FromResult(PreconditionResult.FromError(string.Empty));
+            var res = await base.CheckPermissionsAsync(context, command, services);
+            if (!res.IsSuccess) return res;
 
-            var settings = services.GetRequiredService<Settings.Deserialized.Settings>();
-            if (!settings.AutoThreadChannels.Any(x => currentThread.ParentChannel.Id == x.Id && x.CanArchive))
-                return await Task.FromResult(PreconditionResult.FromError(string.Empty));
+            if (this._autoThreadChannel.CanArchive) return await Task.FromResult(PreconditionResult.FromSuccess());
 
-            var user = (SocketGuildUser)context.Message.Author;
-            if (user.GuildPermissions.ManageThreads) return await Task.FromResult(PreconditionResult.FromSuccess());
+            Task task = context.Message.DeleteAfterSeconds(seconds: 10);
+            return await Task.FromResult(PreconditionResult.FromError("This command cannot be used in a this thread."));
+        }
+    }
 
-            var messages = await currentThread.GetPinnedMessagesAsync();
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class RequireDeletableAutoThreadAttribute : RequireAutoThreadAttribute
+    {
+        public override async Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            var res = await base.CheckPermissionsAsync(context, command, services);
+            if (!res.IsSuccess) return res;
+
+            if (this._autoThreadChannel.CanDelete) return await Task.FromResult(PreconditionResult.FromSuccess());
+
+            Task task = context.Message.DeleteAfterSeconds(seconds: 10);
+            return await Task.FromResult(PreconditionResult.FromError("This command cannot be used in a this thread."));
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class RequireAutoThreadAuthorAttribute : RequireAutoThreadAttribute
+    {
+        public override async Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            var res = await base.CheckPermissionsAsync(context, command, services);
+            if (!res.IsSuccess) return res;
+
+            var messages = await this._currentThread.GetPinnedMessagesAsync();
             var firstMessage = messages.LastOrDefault();
+            if (firstMessage != null)
+            {
+                var user = (SocketGuildUser)context.Message.Author;
+                if (firstMessage.MentionedUsers.Any(x => x.Id == context.User.Id))
+                    return await Task.FromResult(PreconditionResult.FromSuccess());
+            }
 
-            if (firstMessage == null) return await Task.FromResult(PreconditionResult.FromError(string.Empty));
-
-            if (!firstMessage.MentionedUsers.Any(x => x.Id == context.User.Id)) return await Task.FromResult(PreconditionResult.FromError(string.Empty));
-
-            return await Task.FromResult(PreconditionResult.FromSuccess());
+            Task task = context.Message.DeleteAfterSeconds(seconds: 10);
+            return await Task.FromResult(PreconditionResult.FromError("This command can only be used by the thread author."));
         }
     }
 }
