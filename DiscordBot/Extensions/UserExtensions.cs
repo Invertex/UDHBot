dd3eca700720ae661995ad1b657cdda95e5fdbe1
James Kellie <james_kellie@live.com>
2022-10-16 12:52:35 +1000

    HasRoleGroup extension added

--- DiscordBot/Extensions/UserExtensions.cs
+++ DiscordBot/Extensions/UserExtensions.cs
@@ -1,3 +1,5 @@
+using Discord.WebSocket;
+
 namespace DiscordBot.Extensions;
 
 public static class UserExtensions
@@ -5,5 +7,14 @@ public static class UserExtensions
     public static bool IsUserBotOrWebhook(this IUser user)
     {
         return user.IsBot || user.IsWebhook;
+    }
+    
+    public static bool HasRoleGroup(this IUser user, SocketRole role) 
+    {
+        return HasRoleGroup(user, role.Id);
+    }
+    public static bool HasRoleGroup(this IUser user, ulong roleId)
+    {
+        return user is SocketGuildUser guildUser && guildUser.Roles.Any(x => x.Id == roleId);
     }
 }
